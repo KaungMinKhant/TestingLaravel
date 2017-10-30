bb6666e5e1a0deef7ec7086a7dfc660035559930
KaungMinKhant <phenomenalkaung@gmail.com>
2017-10-31 03:30:56 +0630

    First Auth

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 
 //TestController Route
 Route::get('test', 'TestController@index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
